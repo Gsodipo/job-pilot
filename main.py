f52fadae5a79af0336952d5ae4a96200e2c516c9
Grant Sodipo <grantsodipo@gmail.com>
2026-01-06 08:46:09 +0000

    Fix: updated middleware in main.py for vercel

--- main.py
+++ main.py
@@ -29,7 +29,13 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "https://job-pilot-beta.vercel.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "chrome-extension://kdnjioofncjahajjofjokjcekldeac",
+    ],
+    allow_origin_regex=r"^https://.*\.vercel\.app$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
